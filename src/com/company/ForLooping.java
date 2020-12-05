553b19cdce95ebb2a93d91c7bebbe277ce53543a
DamasMahardi <damasmahardi234@gmail.com>
2020-12-05 19:03:47 +0700

    First Project

---
+++ src/com/company/ForLooping.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ForLooping {
+    public static void main(String[] args) {
+        int i ;
+        for (i=0; i<10; i++){
+            System.out.println("Belajar Looping Java");
+            System.out.println("Value of i= "+ i);
+        }
+    }
+}
